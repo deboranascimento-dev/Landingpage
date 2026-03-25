2120e98257544da698f5c29149ca0c3e9636d40f
nascimentodeborapro-pixel <nascimentodeborapro@gmail.com>
2026-03-24 21:26:54 -0300

    organização
    organizando o html e css

--- script.js
+++ script.js
@@ -2,66 +2,55 @@ const btnmobile = document.querySelector(".btn-mobile");
 const navlinks = document.getElementById("nav_links");
 const icon = document.querySelector(".btn-mobile i");
 
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-const items = document.querySelectorAll('.item');
-const dots = document.querySelectorAll('.dot');
-const numberIndicator = document.querySelector('.numbers');
-const homeSection = document.getElementById('home');
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const items = document.querySelectorAll(".item");
+const dots = document.querySelectorAll(".dot");
+const numberIndicator = document.querySelector(".numbers");
+const homeSection = document.getElementById("home");
+const homeBtn = document.querySelectorAll("btn");
 
 btnmobile.addEventListener("click", () => {
-    navlinks.classList.toggle('show');
-    icon.classList.toggle('fa-times');
-    icon.classList.toggle('fa-bars');
+  navlinks.classList.toggle("show");
+  icon.classList.toggle("fa-times");
+  icon.classList.toggle("fa-bars");
 });
 
 let active = 0;
 const total = items.length;
 let timer;
 
-function update(direction) {
-   document.querySelector('.item.active').classList.remove('active')
-   document.querySelector('.dot.active').classList.remove('active')
+function updateSlides(direction) {
+  document.querySelector(".item.active").classList.remove("active");
+  document.querySelector(".dot.active").classList.remove("active");
 
-   if(direction > 0){
-    active = active + 1
+  if (direction > 0) {
+    active = active + 1;
 
-    if( active === total){
-        active = 0
+    if (active === total) {
+      active = 0;
     }
+  } else if (direction < 0) {
+    active = active - 1;
 
-   }
-
-   else if(direction < 0){
-    active = active -1
-
-    if(active < 0){
-        active = total -1
+    if (active < 0) {
+      active = total - 1;
     }
+  }
 
-
-   }
-
-   items[active].classList.add('active')
-   dots[active].classList.add('active')
-
-   numberIndicator.textContent = String(active +1).padStart(2, '0')
- 
-
-
+  items[active].classList.add("active");
+  dots[active].classList.add("active");
 }
 
-clearInterval(timer)
- timer = setInterval(() => {
-    update(1)
+clearInterval(timer);
+timer = setInterval(() => {
+  updateSlides(1);
 }, 5000);
 
-
-
-prevButton.addEventListener('click', () => {
-    update(-1)
+prevButton.addEventListener("click", () => {
+  updateSlides(-1);
 });
 
-nextButton.addEventListener('click', () => {
-   update(1)
+nextButton.addEventListener("click", () => {
+  updateSlides(1);
 });
